4dac31cc6c114a3d02e50c1724d7d12aeaf65a2d
jdm79 <james.malvern@gmail.com>
2018-12-17 16:00:58 +0000

    test: adds a test for proptypes on the congrats component, adds checkProps function in testUtils

--- src/test/Congrats.test.js
+++ src/test/Congrats.test.js
@@ -2,17 +2,18 @@ import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 
-import { findByTestAttr } from './testUtils'
+import { findByTestAttr, checkProps } from './testUtils'
 import Congrats from '../components/Congrats'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
+// props default is empty obj, which is what will return when prop = false
 const setup = ( props={} ) => {
   // spread operator takes props and turns into key/value pairs
   return shallow(<Congrats {...props} />)
 } 
 
-
+// 
 test('renders without error', () => {
   const wrapper = setup()
   const component = findByTestAttr(wrapper, 'component-congrats')
@@ -29,4 +30,9 @@ test('renders non-empty congrats text when success prop is true', () => {
   const wrapper = setup({ success: true })
   const message = findByTestAttr(wrapper, 'congrats-message')
   expect(message.text()).not.toBe(0)
+})
+
+test('does not throw a warning with expected props', () => {
+  const expectedProps = { success: false}
+  checkProps(Congrats, expectedProps)
 })
--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -1,6 +1,17 @@
 // this helper function can be imported into all our test files
 // #DRY
+import checkPropTypes from 'check-prop-types'
+
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test='${val}']`)
+}
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    'prop',
+    component.name
+    )
+    expect(propError).toBeUndefined()
 }
