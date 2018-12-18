6c0b872047d048833e55de6ffc2865400ab06af9
jdm79 <james.malvern@gmail.com>
2018-12-18 21:38:03 +0000

    style: adds bootstrap styling to all the components

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,10 @@ import GuessedWords from './GuessedWords'
 class App extends Component {
   render() {
     return(
-      <div className="App">
+      <div className="container">
         <h1>Guess the Word!</h1>
-        <Congrats success={ false } />
-        <GuessedWords guessedWords={ [] } />
+        <Congrats success={ true } />
+        <GuessedWords guessedWords={ [{ guessedWord: 'train', letterMatchCount: 3}] } />
       </div>
     )
   }
