6f2eb88485e96616081bf76e641bb673dc6c67fe
jdm79 <james.malvern@gmail.com>
2018-12-19 16:16:17 +0000

    chore: adds the shell code for actions and action creator - enough to start the tests

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+// this is where we keep all the action variables
+export const actionTypes = {
+  CORRECT_GUESS: 'CORRECT_GUESS',
+}
+
+// our action creator for correct guesses
+// redux actions are objects that have a type key and sometimes have some kind of payload
+export function correctGuess() {
+  return {}
+}
