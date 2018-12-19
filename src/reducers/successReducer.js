f063857fe71d3884d923a0f03d9a251c887d0e87
jdm79 <james.malvern@gmail.com>
2018-12-19 18:04:10 +0000

    feat: adds passing code for success reducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,4 +1,10 @@
-// shell of the reducer for now
-export default (state, action) => {
-  return null
+import { actionTypes } from '../actions'
+// state is false by default
+export default (state=false, action) => {
+  switch(action.type) {
+    case (actionTypes.CORRECT_GUESS):
+      return true
+    default:
+      return state
+  }
 }
